611bd82901404470911e2a2fddbcc46aca877967
jasdipsekhon <jasdipsekhon@gmail.com>
2021-08-09 20:29:57 -0700

    Create main.cpp

---
+++ Pointers/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 5;
+    int *p;
+    p = &a;
+    cout << a << endl; // value of a
+    cout << &a << endl; // address of a
+    cout << p << endl; // address of a
+    cout << &p << endl; // adddress of p
+    cout << *p << endl; // value of a
+    *p = 10;
+    cout << a << endl;
+    int **q = &p;
+    cout << *q << endl; // address of a
+    cout << **q << endl; // value of a
+    return 0;
+} 
