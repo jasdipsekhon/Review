52f9bd9628c4c67ce8b6b13be2d7d3dca1d21056
jasdipsekhon <jasdipsekhon@gmail.com>
2023-12-30 19:25:15 -0800

    Update bit_manipulation.c

--- C/bit_manipulation.c
+++ C/bit_manipulation.c
@@ -7,6 +7,27 @@ void swap_bits(uint32_t *num, uint32_t pos1, uint32_t pos2) {
     *num = *num ^ ((bit1 << pos1) | (bit2 << pos2));
 }
 
+uint32_t reverseBits(uint32_t n) { // 000101
+    uint32_t reversed;             // 000000 
+    for (int i = 0; i < 32; i++) {
+        reversed = reversed << 1;  // 001010 // shift 32 times to reverse
+        reversed |= (n & 1);       // 000001 // set LSB
+        n = n >> 1;                // 000000 // right shift LSB of n
+    }
+    return reversed;
+}
+
+int hammingWeight(uint32_t n) {
+    int count = 0;
+    for (int i = 0; i < 32; i++) {
+        if ((n & 1) == 1) {
+            count++;
+        }
+        n = n >> 1;
+    }
+    return count;
+}
+
 int main()
 {
     uint32_t n = 0b0100100100100100100100100100101;
